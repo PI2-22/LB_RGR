72cc31e0a2d04b48ed4551356df2a82e5342814c
deLone26 <dan.shebalkoff@yandex.ru>
2025-03-10 00:25:16 +0300

    Реализовал работу с БД (ввод, вывод и т.д.)

--- Form1.cs
+++ Form1.cs
@@ -12,7 +12,7 @@ using Npgsql;
 
 namespace WindowsFormsApp1
 {
-    public partial class Form1: Form
+    public partial class Form1 : Form
     {
         string connectionString = "Server=localhost; Port=5432; Database=rgr; User Id = postgres; Password=43898362Dd+-;";
         public Form1()
@@ -46,6 +46,108 @@ namespace WindowsFormsApp1
 
             command.Dispose();
             sqlConnection.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NpgsqlConnection sqlConnection = new NpgsqlConnection(connectionString);
+            sqlConnection.Open();
+            NpgsqlCommand command = new NpgsqlCommand();
+            command.Connection = sqlConnection;
+            command.CommandType = CommandType.Text;
+            command.CommandText = String.Format("INSERT INTO cost_of_installing_hbo(car_brand,number_of_cylinders,cylinder_capacity,price,installation_cost,model_gbo,manufacturer) VALUES ('{0}', '{1}', '{2}','{3}', '{4}', '{5}', '{6}')", textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text, textBox5.Text, textBox6.Text, textBox7.Text);
+
+            NpgsqlDataReader dataReader = command.ExecuteReader();
+            if (dataReader.HasRows)
+            {
+                DataTable data = new DataTable();
+                data.Load(dataReader);
+                dataGridView1.DataSource = data;
+            }
+
+            command.Dispose();
+            sqlConnection.Close();
+            SqlConnectionReader();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            NpgsqlConnection sqlConnection = new NpgsqlConnection(connectionString);
+            sqlConnection.Open();
+            NpgsqlCommand command = new NpgsqlCommand();
+            command.Connection = sqlConnection;
+            command.CommandType = CommandType.Text;
+            command.CommandText = String.Format("UPDATE cost_of_installing_hbo SET car_brand = '{0}' ,number_of_cylinders = '{1}' ,cylinder_capacity = '{2}' ,price = '{3}' ,installation_cost = '{4}' ,model_gbo = '{5}',manufacturer = '{6}' WHERE cost_id = '{7}'", textBox14.Text, textBox13.Text, textBox12.Text, textBox11.Text, textBox10.Text, textBox9.Text, textBox8.Text, Convert.ToInt32(textBox15.Text));
+
+            NpgsqlDataReader dataReader = command.ExecuteReader();
+            if (dataReader.HasRows)
+            {
+                DataTable data = new DataTable();
+                data.Load(dataReader);
+                dataGridView1.DataSource = data;
+            }
+
+            command.Dispose();
+            sqlConnection.Close();
+            SqlConnectionReader();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            NpgsqlConnection sqlConnection = new NpgsqlConnection(connectionString);
+            sqlConnection.Open();
+            NpgsqlCommand command = new NpgsqlCommand();
+            command.Connection = sqlConnection;
+            command.CommandType = CommandType.Text;
+            command.CommandText = String.Format("DELETE FROM cost_of_installing_hbo WHERE cost_id = '{0}'", Convert.ToInt32(textBox16.Text));
+
+            NpgsqlDataReader dataReader = command.ExecuteReader();
+            if (dataReader.HasRows)
+            {
+                DataTable data = new DataTable();
+                data.Load(dataReader);
+                dataGridView1.DataSource = data;
+            }
+
+            command.Dispose();
+            sqlConnection.Close();
+            SqlConnectionReader();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            NpgsqlConnection sqlConnection = new NpgsqlConnection(connectionString);
+            sqlConnection.Open();
+            NpgsqlCommand command = new NpgsqlCommand();
+            command.Connection = sqlConnection;
+            command.CommandType = CommandType.Text;
+            command.CommandText = String.Format("SELECT * FROM cost_of_installing_hbo WHERE cost_id = '{0}'", Convert.ToInt32(textBox17.Text));
+
+            NpgsqlDataReader dataReader = command.ExecuteReader();
+            if (dataReader.HasRows)
+            {
+                DataTable data = new DataTable();
+                data.Load(dataReader);
+                dataGridView1.DataSource = data;
+            }
+
+            command.Dispose();
+            sqlConnection.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SqlConnectionReader();
         }
     }
 }
